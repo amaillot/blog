5cf95c3e982739e854f96ab2861de801230636d9
amaillot <amaillot01@gmail.com>
2017-03-03 14:52:00 +0100

    Commit qui sert à rien, mais au moins c'est safe

--- src/JoliBlogBundle/Controller/DefaultController.php
+++ src/JoliBlogBundle/Controller/DefaultController.php
@@ -4,14 +4,15 @@ namespace JoliBlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/{name}/{ville}/{temps}")
      */
-    public function indexAction()
+    public function indexAction(string $name, string $ville, int $temps): Response
     {
-        return $this->render('JoliBlogBundle:Default:index.html.twig');
+        return $this->render('JoliBlogBundle:Default:index.html.twig', ['name' => $name, 'ville' => $ville, 'temps' => $temps]);
     }
 }
