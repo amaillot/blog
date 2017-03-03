e98e7749d4c9572cca91eec6d68e693bc079fe4a
amaillot <amaillot01@gmail.com>
2017-03-03 17:07:44 +0100

    Exo

--- src/JoliBlogBundle/Controller/DefaultController.php
+++ src/JoliBlogBundle/Controller/DefaultController.php
@@ -5,14 +5,28 @@ namespace JoliBlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/{name}/{ville}/{temps}")
+     * @Route("/{name}", name="ma_jolie_route")
      */
-    public function indexAction(string $name, string $ville, int $temps): Response
+    public function indexAction(string $name)
     {
-        return $this->render('JoliBlogBundle:Default:index.html.twig', ['name' => $name, 'ville' => $ville, 'temps' => $temps]);
+      $router = $this->get('router');
+      $url = $router->generate('ma_jolie_route', ['name' => $name], UrlGeneratorInterface::ABSOLUTE_URL);
+      if ($name == 'google') {
+        return $this->redirect('http://google.fr');
+      }elseif ($name == 'facebook') {
+        return $this->redirect('http://facebook.fr');
+      }elseif ($name == 'twitter') {
+        return $this->redirect('http://twitter.fr');
+      }else {
+        return $this->render('JoliBlogBundle:Default:index.html.twig', [
+          'name' => $name,
+          'url' => $url
+        ]);      }
+
     }
 }
